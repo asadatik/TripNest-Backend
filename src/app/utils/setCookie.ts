2a6ac8fde1bbf03b025df955d63f896f91eb36b3
Asad Atik <asadatik1995@gmail.com>
2025-12-11 18:15:14 +0600

    cors configaration fixed

--- src/app/utils/setCookie.ts
+++ src/app/utils/setCookie.ts
@@ -11,9 +11,9 @@ export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
   if (tokenInfo.accessToken) {
     res.cookie("accessToken", tokenInfo.accessToken, {
       httpOnly: true,
-      secure: isProd, // ⭐ production এ true, লোকালে false
-      sameSite: isProd ? "none" : "lax", // ⭐ cross-site এর জন্য none
-      // domain set কোরো না Render এ, default থাকুক [web:318][web:324]
+      secure: isProd, //
+      sameSite: isProd ? "none" : "lax", // 
+
     })
   }
 
